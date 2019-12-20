b0f8a120f0c87ae16cda76f331fc26fdac7a2abe
restmad <restmad@outlook.com>
2019-12-20 16:32:48 +0800

    初始化项目

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MailSendReceipt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1088, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送邮件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(65, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1005, 44);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "restmad@outlook.com;huihuangf@isoftstone.com;jmwangx@isoftstone.com;";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(65, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1005, 44);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "看看谁看了邮件了";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(65, 313);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1339, 551);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1439, 876);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Windows.Forms;
+using OpenPop.Pop3;
+
+namespace MailSendReceipt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //这是收取回执的邮箱
+            var Receipt = "";
+
+            //实例化两个必要的
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+
+            //发送邮箱地址
+            mail.From = new MailAddress("");
+
+            //收件人
+            mail.To.Add(new MailAddress(""));
+            mail.To.Add(new MailAddress(""));
+            mail.To.Add(new MailAddress(""));
+
+            //    mail.To.Add(new MailAddress("ruimaj@isoftstone.com"));
+
+            //是否以HTML格式发送
+            mail.IsBodyHtml = true;
+            //主题的编码格式
+            mail.SubjectEncoding = Encoding.UTF8;
+            //邮件的标题
+            mail.Subject = "测试一下发件的标题" + Guid.NewGuid().ToString();
+
+            textBox2.Text = mail.Subject;
+            //内容的编码格式
+            mail.BodyEncoding = Encoding.UTF8;
+            //邮件的优先级
+            mail.Priority = MailPriority.Normal;
+            mail.ReplyToList.Add("");
+            //发送内容,带一个图片标签,用于对方打开之后,回发你填写的地址信息
+            mail.Body = @"邮件通知";
+
+            //发件邮箱的服务器地址
+            smtp.Host = "";
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Timeout = 1000000;
+            //是否为SSL加密
+            smtp.EnableSsl = true;
+            //设置端口,如果不设置的话,默认端口为25
+            smtp.Port = 25;
+            smtp.UseDefaultCredentials = true;
+            //验证发件人的凭据
+            mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess | DeliveryNotificationOptions.OnFailure | DeliveryNotificationOptions.Delay;
+            smtp.Credentials = new System.Net.NetworkCredential("", "");
+
+
+            mail.Headers.Add("ReturnReceipt", "1");
+            mail.Headers.Add("Disposition-Notification-To", "");
+            //    mail.Headers.Add("Return-Receipt-To", "ruimaj@isoftstone.com");
+            try
+            {
+                //发送邮件
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pop3Client client = new Pop3Client();
+
+            if (client.Connected)
+            {
+                client.Disconnect();
+            }
+
+            List<string> accountList = new List<string>();
+            // Connect to the server, false means don't use ssl
+            client.Connect("", 110, false);
+
+            // Authenticate ourselves towards the server by email account and password
+            client.Authenticate("", "");
+
+            //email count
+            int messageCount = client.GetMessageCount();
+
+            //i = 1 is the first email; 1 is the oldest email
+            for (int i = 1; i <= messageCount; i++)
+            {
+                var header = client.GetMessageHeaders(i);
+                if (header.Subject.Contains(textBox2.Text))
+                {
+                    string mail = header.From.Address;
+                    if (!accountList.Contains(mail))
+                    {
+                        accountList.Add(mail);
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string account in accountList)
+            {
+                sb.Append(account + "已阅\n");
+            }
+
+            richTextBox1.Text = sb.ToString();
+        }
+    }
+}
